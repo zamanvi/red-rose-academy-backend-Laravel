dce865702c6d36b42877e6caada418a15cf68afe
zamanvi <norozzaman996@gmail.com>
2026-06-26 11:57:48 +0600

    Temp: add genkey script

---
+++ public/genkey.php
@@ -0,0 +1,9 @@
+﻿<?php
+require __DIR__ . '/../vendor/autoload.php';
+$app = require __DIR__ . '/../bootstrap/app.php';
+$app->make(Illuminate\Contracts\Console\Kernel::class)->bootstrap();
+use Illuminate\Support\Facades\Crypt;
+$value = 'ThisisanencryptedpublickeyforRedRoseWebApplication';
+$encrypted = Crypt::encryptString($value);
+// Remove trailing = signs for URL safety
+echo json_encode(['key' => $encrypted, 'url_safe' => rtrim($encrypted, '=')]);
